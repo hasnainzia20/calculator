877c2f02fc39f65f1910813220d521ee18fa079f
hasnainzia20 <hasnainzia438@gmail.com>
2022-03-04 15:37:24 +0500

    commit4

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let del = document.getElementById('del');
 let operator = document.getElementsByClassName('operation');
 let number = document.getElementsByClassName('numbers');
 
-console.log(Date);
 
 function clrFullScreen() {
     screenVal.value = null;
@@ -49,15 +48,16 @@ function time() {
     document.getElementById("sec").innerHTML = ((d.getSeconds() < 10) ? '0' : '') + d.getSeconds() + ":";
     // document.getElementById("sec").innerHTML = d.getSeconds();
     // if(d.getSeconds() < 10 ) {
-    //     document.getElementById("sec").innerHTML = '0' + d.getSeconds();
-    // }
-    d.toLocaleString('en-US', { hour: 'numeric', hour12: true })
-    if(d.getHours() < 12) {
-        document.getElementById("ampm").textContent = "AM"; 
-    }else {
-        document.getElementById("ampm").textContent = "PM"; 
-        document.getElementById("hour").innerHTML = parseInt(document.getElementById("hour").innerText) - 12 + ":";
-    }
+        //     document.getElementById("sec").innerHTML = '0' + d.getSeconds();
+        // }
+        // d.toLocaleString('en-US', { hour: 'numeric', hour12: true })
+        if(d.getHours() < 12) {
+            document.getElementById("ampm").textContent = "AM"; 
+        }    
+        else {
+            document.getElementById("hour").innerHTML = '0' + parseInt(document.getElementById("hour").innerHTML) - 12 + ":";
+            document.getElementById("ampm").textContent = "PM"; 
+        }
 
 }
 
